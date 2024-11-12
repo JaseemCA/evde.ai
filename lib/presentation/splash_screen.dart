569aaa50f284e5303db158ec73bf5019363f180c
JaseemCA <147582494+JaseemCA@users.noreply.github.com>
2024-11-12 13:09:22 +0530

    started  splash screen and login

---
+++ lib/presentation/splash_screen.dart
@@ -0,0 +1,32 @@
+import 'package:evdeai/presentation/auth/login.dart';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(const Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => LoginPage()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text(
+          'Splash Screen',
+          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+        ),
+      ),
+    );
+  }
+}
